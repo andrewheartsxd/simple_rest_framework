a9d2e004dd3a77cad9edf3368288463804a822a3
andrewheartsXD <andrewheartsxd@users.noreply.github.com>
2015-02-15 23:30:37 -0800

    post and get working

--- index.js
+++ index.js
@@ -2,11 +2,5 @@
 
 var server = require('./lib/server');
 var router = require('./lib/router');
-//var requestHandlers = require('./lib/requestHandlers');
 
-//var handle = {};
-//handle['/'] = requestHandlers.base;
-//handle['/superheroes'] = requestHandlers.superheroes;
-
-//server.start(router.route, handle);
 server.start(router.route);
