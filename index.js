3f335f0d537e1aa72cd9f0b52bb312e5b9f65796
andrewheartsXD <andrewheartsxd@users.noreply.github.com>
2015-02-16 22:26:15 -0800

    need to get validpaths working

--- index.js
+++ index.js
@@ -2,7 +2,28 @@
 
 var server = require('./lib/server');
 var router = require('./lib/router');
+var folderMaker = require('./lib/folderCheck');
 
-var addResource = 'superheroes';
+var validPaths = [];
 
-server.start(router.route, addResource);
+function addResource(input) {
+  folderMaker.makeFolder(input,  function(err) {
+    if (err) { 
+      throw err; 
+    } else { 
+      validPaths.push('/' + input);
+      console.log('Resource added');
+      console.log(validPaths);
+    }
+  });
+}
+
+function startServer() {
+  console.log(validPaths);
+  server.start(router.route);
+};
+
+exports.addResource = addResource;
+exports.startServer = startServer;
+
+
--- test/testing.js
+++ test/testing.js
@@ -1,48 +1,96 @@
 'use strict';
 
-require('../index');
+require('../app');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var fs = require('fs');
 
 chai.use(chaiHttp);
 
 var expect = chai.expect;
 
+
 describe('our http server', function() {
-  var server = 'localhost:3000';
+  describe('all', function() {
 
-  it('should respond to a blank request', function(done) {
-    chai.request(server)
-    .get('/')
-    .end(function(err, res) {
-      expect(err).to.equal(null);
-      expect(res).to.have.status(200);
-      expect(res.text).to.equal('ohaider');
-      done();
+    var fileCount;
+    fs.readdir('./superheroes/data/', function(err, files) {
+      if(err){
+        throw err;
+      }
+      else {
+        fileCount = files.length;
+        console.log('FILECOUUUUUNT: ' + fileCount);
+      }
     });
-  });
 
+    var server = 'localhost:3000';
 
-  it('should respond to a time request', function(done) {
-    chai.request(server)
-    .get('/time')
-    .end(function(err, res) {
-      expect(err).to.equal(null);
-      expect(res).to.have.status(200);
-      expect(Date.parse(res.text)).to.be.closeTo(Date.now(), 100000);
-      done();
+    it('should respond to a POST request', function(done) {
+      chai.request(server)
+      .post('/superheroes')
+      .send({hero: "cyclops", identity: "scott summers"})
+      .end(function(err, res) {
+        expect(err).to.equal(null)
+        expect(res).to.have.status(200)
+        expect(res.text).to.equal(JSON.stringify({msg: "File created!"}))
+        done();
+      })
     });
-  });
-  
-  it('should respond to a greet request', function(done) {
-    chai.request(server)
-    .get('/greet/yourname')
-    .end(function(err, res) {
-      expect(err).to.equal(null);
-      expect(res).to.have.status(200);
-      expect(res.text).to.equal("'sup yourname?");
-      done();
+
+    it('should respond to a GET request', function(done) {
+      chai.request(server)
+      .get('/superheroes/cyclops')
+      .end(function(err, res) {
+        expect(err).to.equal(null)
+        expect(res).to.have.status(200)
+        fs.readFile('./superheroes/data/cyclops.json', function(err, data) {
+          if (err) throw err;
+          expect(res.text).to.equal(data.toString());
+          done();
+        })
+      })
+    });
+    
+    it('should respond to a PUT request', function(done) {
+      chai.request(server)
+      .put('/superheroes/cyclops')
+      .send({hero: "cable", identity: "nathan summers"})
+      .end(function(err, res) {
+        expect(err).to.equal(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal(JSON.stringify({hero: "cable", identity: "nathan summers"}))
+        done();
+      })
+    });
+
+    it('should respond to a PATCH request', function(done) {
+      chai.request(server)
+      .patch('/superheroes/cyclops')
+      .send({hero: "xman", identity: "nathan summers", team: "xmen"})
+      .end(function(err, res) {
+        expect(err).to.equal(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal(JSON.stringify({hero: "xman", identity: "nathan summers", team: "xmen"}))
+        done();
+      })
+    });
+
+    it('should respond to a DELETE request', function(done) {
+      chai.request(server)
+      .delete('/superheroes/cyclops')
+      .end(function(err, res) {
+        expect(err).to.equal(null);
+        expect(res).to.have.status(200);
+        fs.readdir('./superheroes/data/', function(err, files) {
+          if(err) throw err;
+          expect(files.length).to.equal(fileCount);
+          done();
+        });
+      });
     });
   });
 });
+
+  
 
